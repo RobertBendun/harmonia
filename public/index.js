d9f692a3461421178594eedf23596d7f758d18ae
Robert Bendun <robert@bendun.cc>
2023-09-16 19:18:02 +0200

    MIDI information exchange between JS and server

--- public/index.js
+++ public/index.js
@@ -1,3 +1,41 @@
+/** @type{HTMLDivElement} */
+let midi_sources_div = null;
+
+document.addEventListener('DOMContentLoaded', () => {
+	const input = document.getElementById('midi-sources-input');
+	input.addEventListener('change', async function() {
+		/** @type{FileList} */
+		const fileList = this.files;
+
+		const formData = new FormData();
+		for (const file of fileList) {
+			formData.append('files[]', file);
+		}
+		const response = await fetch('/add-midi-sources', { method: 'POST', body: formData });
+		for (const midi_source of await response.json()) {
+			const tile = document.createElement('div');
+			if (midi_source.Ok) {
+				const { file_name, format, tracks_count, uuid } = midi_source.Ok;
+				tile.setAttribute('data-uuid', uuid);
+				tile.innerHTML = `<h3>${file_name}</h3><p>Format: ${format}, tracks count: ${tracks_count}</p>`;
+				const btn = document.createElement('button');
+				btn.innerText = 'Play';
+				btn.addEventListener('click', async () => {
+					const response = await fetch(`/play/${uuid}`, { method: 'POST' });
+					const data = await response.text();
+					console.log(data);
+				});
+				tile.appendChild(btn);
+			} else {
+				tile.innerHTML = `<strong>ERROR</strong>: ${midi_source.Err}`;
+			}
+			midi_sources_div.appendChild(tile);
+		}
+	});
+
+	midi_sources_div = document.getElementById('midi-sources-list');
+});
+
 // Create WebSocket connection.
 const socket = new WebSocket(`ws://${location.host}/ws`);
 
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
-use std::{net::SocketAddr, path::PathBuf};
+use std::{net::SocketAddr, path::PathBuf, sync::{Arc, RwLock}};
 
 use axum::{
     extract::{
         ws::{Message, WebSocket},
-        ConnectInfo, WebSocketUpgrade,
+        ConnectInfo, Multipart, Path, WebSocketUpgrade,
     },
     response::IntoResponse,
-    routing::get,
-    Router, TypedHeader,
+    routing::{get, post},
+    Json, Router, TypedHeader, Extension,
 };
+use midly::Smf;
 use tower_http::{
     services::ServeDir,
     trace::{DefaultMakeSpan, TraceLayer},
@@ -16,8 +17,12 @@ use tower_http::{
 use tracing::{error, info};
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
+type MidiSources = std::collections::HashMap<String, ()>;
+
 #[tokio::main]
 async fn main() {
+    let midi_sources = Arc::new(RwLock::new(MidiSources::new()));
+
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
@@ -31,10 +36,13 @@ async fn main() {
     let app = Router::new()
         .fallback_service(ServeDir::new(public_dir).append_index_html_on_directories(true))
         .route("/ws", get(websocket_handler))
+        .route("/add-midi-sources", post(add_midi_sources_handler))
+        .route("/play/:uuid", post(play_handler))
         .layer(
             TraceLayer::new_for_http()
                 .make_span_with(DefaultMakeSpan::default().include_headers(true)),
-        );
+        )
+        .layer(Extension(midi_sources));
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
     info!("listening on http://{addr}");
@@ -42,6 +50,62 @@ async fn main() {
         .serve(app.into_make_service_with_connect_info::<SocketAddr>())
         .await
         .unwrap();
+}
+
+async fn play_handler(
+    Path(uuid): Path<String>,
+    midi_sources: Extension<Arc<RwLock<MidiSources>>>,
+) {
+    if midi_sources.read().unwrap().contains_key(&uuid) {
+        println!("playing {uuid}");
+    } else {
+        println!("not found");
+    }
+}
+
+#[derive(serde::Serialize)]
+struct MidiSourceLoaded {
+    file_name: String,
+    format: String,
+    tracks_count: usize,
+    uuid: String,
+}
+
+async fn add_midi_sources_handler(
+    midi_sources: Extension<Arc<RwLock<MidiSources>>>,
+    mut multipart: Multipart,
+) -> Json<Vec<Result<MidiSourceLoaded, String>>> {
+    let mut statuses = vec![];
+
+
+    while let Some(field) = multipart.next_field().await.unwrap() {
+        // TODO: Check that this is the name that we are expecting
+        let _name = field.name().unwrap().to_string();
+        // TODO: Better default file name
+        let file_name = field.file_name().unwrap_or("<unknown>").to_string();
+        let data = field.bytes().await.unwrap().to_vec();
+
+        statuses.push(match Smf::parse(&data) {
+            Ok(midi) => {
+                let uuid = uuid::Uuid::new_v4().to_string();
+                let mut midi_sources = midi_sources.write().unwrap();
+                midi_sources.insert(uuid.clone(), ());
+
+                Ok(MidiSourceLoaded {
+                    file_name,
+                    format: match midi.header.format {
+                        midly::Format::SingleTrack | midly::Format::Sequential => "sequential",
+                        midly::Format::Parallel => "parallel",
+                    }
+                    .to_string(),
+                    tracks_count: midi.tracks.len(),
+                    uuid,
+                })
+            }
+            Err(err) => Err(err.to_string()),
+        });
+    }
+    Json(statuses)
 }
 
 // For expanding this websocket buisness see: https://github.com/tokio-rs/axum/blob/main/examples/websockets/src/main.rs
