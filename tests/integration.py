6afedbb4a8d238dad75f2f6730603bea88fae38b
Robert Bendun <robert@bendun.cc>
2025-03-22 13:59:31 +0100

    Fix integrations tests to new output format

--- tests/integration.py
+++ tests/integration.py
@@ -4,29 +4,47 @@ import signal
 import subprocess
 import unittest
 
+
 @contextlib.contextmanager
 def harmonia():
-    # subprocess.run(["cargo", "build", "--bin", "harmonia"], check=True)
-    with subprocess.Popen(["cargo", "run", "--bin", "harmonia"], stdout=subprocess.PIPE) as p:
+    ip = "127.0.0.1"
+    port = "8888"
+
+    with subprocess.Popen(
+        [
+            "cargo",
+            "run",
+            "--bin",
+            "harmonia",
+            "--",
+            "--ip",
+            ip,
+            "--port",
+            port,
+        ],
+        stdout=subprocess.PIPE,
+    ) as p:
+        address = b"http://%s:%s" % (ip.encode("utf-8"), port.encode("utf-8"))
+
         try:
             for line in p.stdout:
                 # That means that Harmonia should be ready to accept connections
-                if b"Listening" in line:
-                    yield
+                if address in line:
+                    yield address
                     break
 
             p.send_signal(signal.SIGINT)
+            p.wait(10)
         except:
             p.kill()
 
 
 class IntegrationTesting(unittest.TestCase):
     def test_connects(self):
-        with harmonia():
-            response = requests.get("http://localhost:8080/midi/ports")
+        with harmonia() as url:
+            response = requests.getf(f"{url}/midi/ports")
             assert response.status_code == 200
             print(response.text)
-
 
 
 if __name__ == "__main__":
